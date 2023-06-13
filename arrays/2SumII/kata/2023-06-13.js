78ae4cefff9ca6e8a9f690934e029bcf9c24b635
tmanley1985 <tmanley1985@gmail.com>
2023-06-13 13:54:53 -0400

    Adding kata

--- arrays/2SumII/kata/2023-06-13.js
+++ arrays/2SumII/kata/2023-06-13.js
@@ -1,7 +1,4 @@
 const twoSumIINaive = (nums, target) => {
-  // Why do we have nums.length - 1 here?
-  // Well think about it, we're comparing each number pair
-  // But we don't want to compare the same number to itself!
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) return [i + 1, j + 1]
@@ -18,10 +15,9 @@ const twoSumII = (nums, target) => {
     const complement = target - nums[i]
 
     if (complement in dp) return [dp[complement] + 1, i + 1]
+
     dp[nums[i]] = i
   }
-
-  return []
 }
 
 module.exports = {
