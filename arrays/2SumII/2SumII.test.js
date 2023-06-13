a4b55ef29ccc8a576139f0087f1d688a20bda1be
tmanley1985 <tmanley1985@gmail.com>
2023-06-13 13:49:25 -0400

    Added new test case

--- arrays/2SumII/2SumII.test.js
+++ arrays/2SumII/2SumII.test.js
@@ -22,7 +22,8 @@ describe('Two Sum II', () => {
             it(`Test ${functionName} from ${fileName}`, () => {
 
                 const testCases = [
-                    {args: [[1, 3, 4, 5, 7, 10, 11], 9 ], expected: [3,4] }
+                    {args: [[1, 3, 4, 5, 7, 10, 11], 9 ], expected: [3,4] },
+                    {args: [[1,2,3,4,5], 8 ], expected: [3,5] }
                 ]
 
                 for (const {args, expected} of testCases) {
