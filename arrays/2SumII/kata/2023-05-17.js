617d4258b644ed4240f35cb656e534297f5f6b77
tmanley1985 <tmanley1985@gmail.com>
2023-06-13 13:46:18 -0400

    Added tests

---
+++ arrays/2SumII/kata/2023-05-17.js
@@ -0,0 +1,27 @@
+const twoSumIINaive = (nums, target) => {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i + 1, j + 1]
+    }
+  }
+
+  return []
+}
+
+const twoSumII = (nums, target) => {
+  const dp = {}
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i]
+
+    if (complement in dp) return [dp[complement] + 1, i + 1]
+    dp[nums[i]] = i
+  }
+
+  return []
+}
+
+module.exports = {
+  twoSumIINaive,
+  twoSumII,
+}
