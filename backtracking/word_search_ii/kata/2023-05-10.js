1d0ddb247ebb29ec80c1bfc2cff6a68739534fba
tmanley1985 <tmanley1985@gmail.com>
2023-06-22 15:28:13 -0400

    Word Search II is tested

--- backtracking/word_search_ii/kata/2023-05-10.js
+++ backtracking/word_search_ii/kata/2023-05-10.js
@@ -1,85 +1,73 @@
-
 const TrieNode = (children = {}, isWord = false) => ({
-    children,
-    isWord,
-    addWord(word) {
-        let current = this 
+  children,
+  isWord,
+  addWord(word) {
+    let current = this
 
-        for (let letter of word) {
-
-            if (!(letter in current.children)) {
-                current.children[letter] = TrieNode()
-            }
-            current = current.children[letter]
-        }
-
-        current.isWord = true
-    },
-    addWords(words) {
-        
-        for (let word of words) {
-            this.addWord(word)
-        }
+    for (let letter of word) {
+      if (!(letter in current.children)) {
+        current.children[letter] = TrieNode()
+      }
+      current = current.children[letter]
     }
+
+    current.isWord = true
+  },
+  addWords(words) {
+    for (let word of words) {
+      this.addWord(word)
+    }
+  },
 })
 
 const findWords = (board, words) => {
+  const trie = TrieNode()
+  trie.addWords(words)
 
-    const trie = TrieNode()
-    trie.addWords(words)
+  const NUM_ROWS = board.length
+  const NUM_COLS = board[0].length
 
-    const NUM_ROWS = board.length
-    const NUM_COLS = board[0].length
+  const seen = new Set()
+  const result = new Set()
 
-    const seen = new Set()
-    const result = new Set()
+  const dfs = (row, column, currentNode, prefix) => {
+    const seenKey = `${row},${column}`
 
-    const dfs = (row, column, currentNode, prefix) => {
+    if (row < 0 || column < 0 || row >= NUM_ROWS || column >= NUM_COLS) return
 
-        const seenKey = `${row},${column}`
+    if (seen.has(seenKey)) return
 
-        if (row < 0 || column < 0 || row >= NUM_ROWS || column >= NUM_COLS) return 
+    let char = board[row][column]
 
-        if (seen.has(seenKey)) return
+    if (!(char in currentNode.children)) return
 
-        let char = board[row][column]
+    seen.add(seenKey)
 
-        if (! (char in currentNode.children)) return
+    let next = currentNode.children[char]
+    let newPrefix = prefix + char
 
-        seen.add(seenKey)
-
-        let next = currentNode.children[char]
-        let newPrefix = prefix + char
-
-        if (next.isWord) {
-            result.add(newPrefix)
-        }
-
-        // Explore
-
-        dfs(row - 1, column, next, newPrefix)
-        dfs(row + 1, column, next, newPrefix)
-        dfs(row, column - 1, next, newPrefix)
-        dfs(row, column + 1, next, newPrefix)
-
-        seen.delete(seenKey)
+    if (next.isWord) {
+      result.add(newPrefix)
     }
 
-    for (let row = 0; row < NUM_ROWS; row++) {
-        
-        for (let column = 0; column < NUM_COLS; column++) {
-        
-            dfs(row, column, trie, "")
-            
-        }
-        
+    // Explore
+
+    dfs(row - 1, column, next, newPrefix)
+    dfs(row + 1, column, next, newPrefix)
+    dfs(row, column - 1, next, newPrefix)
+    dfs(row, column + 1, next, newPrefix)
+
+    seen.delete(seenKey)
+  }
+
+  for (let row = 0; row < NUM_ROWS; row++) {
+    for (let column = 0; column < NUM_COLS; column++) {
+      dfs(row, column, trie, "")
     }
-    return [...result]
+  }
+  return [...result]
 }
 
-const log = console.log
-
-const board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
-const words = ["oath","pea","eat","rain"]
-
-log(findWords(board, words)) // [eat, oath]
+module.exports = {
+  findWords,
+}
---
+++ backtracking/word_search_ii/word-search-ii.test.js
@@ -0,0 +1,41 @@
+
+
+const fs = require('fs');
+const path = require('path');
+const { expect } = require('expect');
+
+describe('Word Search II', () => {
+    const kataFolderPath = path.join(__dirname, 'kata');
+
+    // Get the list of dated files within the kata folder
+    const datedFiles = fs.readdirSync(kataFolderPath);
+
+    // Import and test each dated file
+    datedFiles.forEach((fileName) => {
+        const filePath = path.join(kataFolderPath, fileName);
+        const module = require(filePath);
+
+        // Iterate over the exported functions from the module
+        Object.entries(module).forEach(([functionName, algorithmFunction]) => {
+
+            // Define your test cases for each function from each dated file
+            it(`Test ${functionName} from ${fileName}`, () => {
+
+
+                const board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+                const words = ["oath","pea","eat","rain"]
+
+                const testCases = [
+                    {args: [board, words], expected: ["oath", "eat"] },
+                ]
+
+
+                for (const {args, expected} of testCases) {
+
+                    expect(algorithmFunction(...args)).toEqual(expected)
+                }
+                
+            });
+        });
+    });
+});
