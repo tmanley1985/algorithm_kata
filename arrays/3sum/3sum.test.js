fbfee7f47b58f1e79d1c56ef6d99d2b571924c87
tmanley1985 <tmanley1985@gmail.com>
2023-06-22 14:32:54 -0400

    Three sum test added, remembered most of it

---
+++ arrays/3sum/3sum.test.js
@@ -0,0 +1,40 @@
+
+
+const fs = require('fs');
+const path = require('path');
+const { expect } = require('expect');
+
+describe('Three Sum', () => {
+    const kataFolderPath = path.join(__dirname, 'kata');
+
+    // Get the list of dated files within the kata folder
+    const datedFiles = fs.readdirSync(kataFolderPath);
+
+    // Import and test each dated file
+    datedFiles.forEach((fileName) => {
+        const filePath = path.join(kataFolderPath, fileName);
+        const module = require(filePath);
+
+        // Iterate over the exported functions from the module
+        Object.entries(module).forEach(([functionName, algorithmFunction]) => {
+
+            // Define your test cases for each function from each dated file
+            it(`Test ${functionName} from ${fileName}`, () => {
+
+                const testCases = [
+                    {args: [[3,7,1,2,8,4,5]], expected: [] },
+                    {args: [[0,0,0] ], expected: [[0,0,0]] },
+                    {args: [[-1,0,1,2,-1,-4] ], expected: [[-1,-1,2],[-1,0,1]] },
+                ]
+
+
+                for (const {args, expected} of testCases) {
+                    
+                    // [2,3] becomes [3,4] because of 1 based indexing.
+                    expect(algorithmFunction(...args)).toEqual(expected)
+                }
+                
+            });
+        });
+    });
+});
--- arrays/3sum/kata/2023-05-17.js
+++ arrays/3sum/kata/2023-05-17.js
@@ -1,38 +1,33 @@
+const threeSum = nums => {
+  const answers = []
 
-var threeSum = function(nums, target = 0) {
-       
-    let answer = []
+  if (nums.length < 3) return answer
 
-    if (nums.length < 3) return answer
-    
-    nums = nums.sort((a, b) => a - b)
-    
-    for(let i = 0; i <= nums.length - 3; i++){
+  nums = nums.sort((a, b) => a - b)
 
-        if(i == 0 || nums[i] > nums[i-1]){
-            let start = i + 1;
-            let end = nums.length - 1;
-            
-            while (start < end){
-                if (nums[i] + nums[start] + nums[end] == 0){
-                    answer.push([ nums[i] , nums[start] , nums[end] ]);
-                }
-                if (nums[i] + nums[start] + nums[end] < 0){
-                    let currentStart = start;
-                    while((nums[currentStart] == nums[start]) && start < end) start++;
-                }
-                else {
-                    let currentEnd = end;
-                    while((nums[currentEnd] == nums[end]) && start < end) end--;
-                }
-            }
+  for (let i = 0; i <= nums.length - 3; i++) {
+    if (i === 0 || nums[i] > nums[i - 1]) {
+      let start = i + 1
+      let end = nums.length - 1
+
+      while (start < end) {
+        if (nums[i] + nums[start] + nums[end] === 0) {
+          answers.push([nums[i], nums[start], nums[end]])
         }
+        if (nums[i] + nums[start] + nums[end] < 0) {
+          let currentStart = start
+          while (nums[currentStart] === nums[start] && start < end) start++
+        } else {
+          let currentEnd = end
+          while (nums[currentEnd] === nums[end] && start < end) end--
+        }
+      }
     }
+  }
 
+  return answers
+}
 
-    return answer;
-};
-
-const log = console.log 
-
-log(threeSum([3,7,1,2,8,4,5],20))
+module.exports = {
+  threeSum,
+}
