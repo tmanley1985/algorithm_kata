794655d31b7701834f96c88576c7c70f04f32169
tmanley1985 <tmanley1985@gmail.com>
2023-07-25 14:58:06 -0400

    Added comments and did various data structure kata

--- ds/tries/search_suggestions_system/kata/2023-07-05.js
+++ ds/tries/search_suggestions_system/kata/2023-07-05.js
@@ -1,10 +1,15 @@
 const suggestedProducts = function (products, searchWord) {
+  // We're going to sort the products lexicographically so that the trie
+  // has the lexicographic ordering.
   products.sort()
   const trie = new Trie()
   trie.addWords(products)
 
   let results = []
 
+  // We're simulating a person typing into an input one character at a time.
+  // So we start with an empty word and for each letter we'll push on the words
+  // that we suggest for that prefix.
   let prefix = ""
   for (const letter of searchWord) {
     prefix += letter
@@ -67,13 +72,22 @@ class Trie {
       current = current.children[letter]
     }
 
+    // We have to pass the word we're building up, because we want to know the word
+    // to push onto the results. The easiest way to do that is to keep the word as we build it.
+    // Otherwise we'd have to keep the starting node the path we took to get to the current node.
+    // The secret is that: THE WORD IS THE PATH.
     const dfs = (node, word) => {
+      // Per the problem, we only want to return the first three
+      // results per prefix.
       if (results.length === 3) return
 
       if (node.isWord) {
         results.push(word)
       }
 
+      // This is a standard dfs action here but it may not look like it.
+      // Remember, in dfs we recurse on the children. The way that you get
+      // the children in a trie is looking at the keys in the dictionary it holds.
       for (const letter in node.children) {
         dfs(node.children[letter], word + letter)
       }
