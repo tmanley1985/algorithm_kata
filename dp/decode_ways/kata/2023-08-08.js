a2e398d4f5ac174a692c4d1904c07b7a9491231c
tmanley1985 <tmanley1985@gmail.com>
2023-08-08 16:45:30 -0400

    Added the tabulated kata

--- dp/decode_ways/kata/2023-08-08.js
+++ dp/decode_ways/kata/2023-08-08.js
@@ -20,6 +20,28 @@ const decodeWays = s => {
   return dfs()
 }
 
+const decodeWaysTab = s => {
+  const dp = Array.from({ length: s.length + 1 }).fill(0)
+
+  dp[0] = 1
+  dp[1] = 1
+
+  for (let i = 2; i <= s.length; i++) {
+    const oneChar = s.slice(i - 1, i)
+    const twoChars = s.slice(i - 2, i)
+
+    if (0 < oneChar < 10) {
+      dp[i] += dp[i - 1]
+    }
+    if (9 < twoChars < 27) {
+      dp[i] += dp[i - 2]
+    }
+  }
+
+  return dp[s.length]
+}
+
 module.exports = {
   decodeWays,
+  decodeWaysTab,
 }
